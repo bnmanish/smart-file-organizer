1998b9f9b6998694881e1c0f171217b7d92d95e0
bnmanish <bnmanish006@gmail.com>
2026-08-03 23:55:24 +0530

    update

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-print("Welcome to Smart File Organizer Version : 1.0")
+app_name = "Smart File Organizer"
+version = "1.0"
+author = "B N Manish"
+
+print(f"Welcome to {app_name}")
+print(f"Version : {version}")
+print(f"Author : {author}")

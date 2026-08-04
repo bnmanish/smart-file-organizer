a3b7a047e1b9563a7bb44399b9fe4a90df96f4d4
bnmanish <bnmanish006@gmail.com>
2026-08-04 08:45:16 +0530

    changes

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
 app_name = "Smart File Organizer"
 version = "1.0"
 author = "B N Manish"
+language = "Python"
+purpose = "Organize Files Automatically"
+total_files = 250
+organized_files = 180
+# remaining_files = 70
+remaining_files = total_files - organized_files
+source_folder = "/home/manish/Downloads"
+pdf_count = 10
+image_count = 20
+video_count = 5
 
 print(f"Welcome to {app_name}")
 print(f"Version : {version}")
 print(f"Author : {author}")
+print(f"language : {language}")
+print(f"purpose : {purpose}")
+print(f"Total Files : {total_files}")
+print(f"Organized Files : {organized_files}")
+print(f"Remaining Files : {remaining_files}")
+print(f"Source Folder : {source_folder}")
+print(f"PDF Files : {pdf_count}")
+print(f"image Files : {image_count}")
+print(f"Video Files : {video_count}")
---
+++ user_info.py
@@ -0,0 +1,18 @@
+# ===== Developer Profile =====
+# Name : Manish
+# Experience : 7 Years
+# Favorite Language : PHP
+# =============================
+
+
+name = input("Enter your name: ")
+experience = input("Enter your experience: ")
+language = input("Enter your favorite language: ")
+
+
+
+print(f"===== Developer Profile =====")
+print(f"Name :  {name}")
+print(f"Experience :  {experience} Years")
+print(f"Favorite Language : {language}")
+print(f"=============================")

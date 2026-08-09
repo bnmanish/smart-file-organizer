d75135c10ff5e93db97e80efa65132095ae428fa
bnmanish <bnmanish006@gmail.com>
2026-08-10 00:52:21 +0530

    updates

--- modify_list.py
+++ modify_list.py
@@ -8,3 +8,15 @@ files.append("invoice.pdf")
 files.append("notes.txt")
 
 print(files)
+
+files.remove("photo.jpg")
+
+print(files)
+
+for file in files:
+    print(file)
+
+if "invoice.pdf" in files:
+    print('exist')
+else:
+    print('does not exist')

efc067b138d09f7f2fe198c137c5aa505f2d6eac
bnmanish <bnmanish006@gmail.com>
2026-08-15 23:35:39 +0530

    updates

--- organizer.py
+++ organizer.py
@@ -18,15 +18,40 @@ def get_category(extension):
 		return('Other')
 # ----------------function to get category starts--------------
 
+#------ 1. Duplicate Filename Problem solution starts----------
+def check_unique_destination(destination):
+		i=1
+		while True:
+			filename = f'{destination.stem}_{i}{destination.suffix}'
+			newdes = destination.parent / filename 
+			if not newdes.exists():
+				return newdes
+			else:
+				i = i+1
+				print(i)
+#------ 1. Duplicate Filename Problem solution ends----------
+
 
 #source path whitch file to organize
 source_folder = Path("/home/bnmanish/Downloads/python")
 
 for file in source_folder.iterdir():
 	if file.is_file():
-		print(file)
+		# print(file)
 		category = get_category(file.suffix)
 		fillFolderPath = source_folder / category #dir created to move the files
 		fillFolderPath.mkdir(exist_ok=True)
 		destination = fillFolderPath/file.name
-		file.rename(destination)
+		if not destination.exists():
+			try:
+				file.rename(destination)
+				print(f'{file.name} moved in {category}')
+			except OSError as error:
+				print(f'Failed to move {file.name} in {category} : {error}')
+		else:
+			try:
+				newdest = check_unique_destination(destination)
+				file.rename(newdest)
+				print(f'{file.name} moved in {category}')
+			except OSError as error:
+				 print(f'Failed to move {file.name} in {category} : {error}')

4776e489b8a898f772ca681daff51b814b1421a3
bnmanish <bnmanish006@gmail.com>
2026-08-19 09:26:06 +0530

    report optimization

--- organizer.py
+++ organizer.py
@@ -28,8 +28,7 @@ def check_unique_destination(destination):
 			if not newdes.exists():
 				return newdes
 			else:
-				i = i+1
-				print(i)
+				i += 1
 #------ 1. Duplicate Filename Problem solution ends----------
 
 
@@ -46,21 +45,22 @@ successful_files = 0
 failed_files = 0
 
 #source path whitch file to organize
-dir = input('Enter folder path : ')
-source_folder = Path(dir)
+folder_path = input('Enter folder path : ')
+source_folder = Path(folder_path)
 if not source_folder.is_dir():
 	print('This path does not exist')
 	sys.exit()
 for file in source_folder.iterdir():
 	if file.is_file():
 		category = get_category(file.suffix)
-		fillFolderPath = source_folder / category #dir created to move the files
-		fillFolderPath.mkdir(exist_ok=True)
-		destination = fillFolderPath/file.name
-		if destination.exists():
-			destination = check_unique_destination(destination)
-
+		folder_path = source_folder / category #dir created to move the files
 		try:
+
+			folder_path.mkdir(exist_ok=True)
+			destination = folder_path/file.name
+			if destination.exists():
+				destination = check_unique_destination(destination)
+			
 			file.rename(destination)
 			print(f'{file.name} ====> moved in ====> {category}')
 			counts[category] += 1
@@ -76,17 +76,13 @@ for file in source_folder.iterdir():
 print('=======================================')
 print('      😀ORGANIZATION COMPLETE😎')
 print('=======================================')
-
 print(f'Total Files : {total_files}')
-print(f'PDF         : {counts['PDF']}')
-print(f'Image       : {counts['Image']}')
-print(f'Video       : {counts['Video']}')
-print(f'Document    : {counts['Document']}')
-print(f'Other       : {counts['Other']}')
-print('                                        ')
+print()
+for category, count in counts.items():
+	print(f"{category:<10}: {count}")
+print()
 print(f'Successfully moved : {successful_files}')
 print(f'Failed : {failed_files}')
-
 print('=======================================')
 
 

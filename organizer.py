1cdb5cde0248c6660161be8a2fc3986cb13bc0b8
bnmanish <bnmanish006@gmail.com>
2026-08-29 19:04:56 +0530

    fixed issue to of creating file while preview

--- organizer.py
+++ organizer.py
@@ -29,22 +29,17 @@ for file in source_folder.iterdir():
 		category = get_category(file.suffix)
 		folder_path = source_folder / category #dir created to move the files
 		try:
-
-			folder_path.mkdir(exist_ok=True)
 			destination = folder_path/file.name
 			if destination.exists():
 				destination = check_unique_destination(destination)
 			
-			# file.rename(destination)
-			finalData[i] = destination
-			print(f'{file.name} ====> moved in ====> {category}')
+			finalData[file] = destination
+			print(f'{file.name} ====> to move in ====> {category}')
 			counts[category] += 1
-			successful_files += 1
 			total_files += 1
 			i += 1
 		except OSError as error:
-			print(f'Failed to move {file.name} in {category} : {error}')
-			failed_files += 1
+			print(f'will fail to move {file.name} in {category} : {error}')
 			total_files += 1
 
 print('=======================================')
@@ -58,13 +53,20 @@ print('=======================================')
 
 # pprint(finalData)
 
-confirmation = input("Do you want to organize these files? (y/n): ")
-if confirmation != 'y':
-	print('Thanks for using file organizer')
-	sys.exit()
+confirmation = input("Do you want to organize these files? (y/n): ").lower()
+if confirmation != "y":
+    print("Thanks for using Smart File Organizer")
+    sys.exit()
 
-for value in finalData.values:
-	file.rename(value)
+for file, destination in finalData.items():
+    try:
+        destination.parent.mkdir(exist_ok=True)
+        file.rename(destination)
+        successful_files += 1
+        print(f"{file.name} moved successfully")
+    except OSError as error:
+        print(f"Failed to move {file.name}: {error}")
+        failed_files += 1
 
 print('=======================================')
 print('      😀ORGANIZATION COMPLETE😎')
